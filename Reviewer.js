7346d54df50eade2b0e048800219178107389b99
Eyadsaeda <47503761+Eyadsaeda@users.noreply.github.com>
2019-03-02 11:59:16 +0200

    Add files via upload

---
+++ Reviewer.js
@@ -0,0 +1,13 @@
+class Reviewer {
+    constructor(Name, ID, Email,Username , Password , YearsOfExperience , Gender) {
+        this.Name = Name;
+        this.Email=Email;
+        this.Gender = Gender;
+        this.YearsOfExperience = YearsOfExperience;
+        this.Username= Username;
+        this.Password = Password;
+        this.ID= ID;
+    };
+};
+
+module.exports = Reviewer 
