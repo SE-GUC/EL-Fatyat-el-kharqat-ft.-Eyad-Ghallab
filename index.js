1eb93daf64e73bbcf7c7dbc3fec23a0e7d928454
Eyadsaeda <47503761+Eyadsaeda@users.noreply.github.com>
2019-03-02 12:27:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const reviewers = require('./routes/api/reviewers')
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>reviewers page</h1>
+    <a href="/api/users">reviewers</a>
+    `);
+})
+
+// Direct routes to appropriate files 
+app.use('/api/reviewers', reviewers) 
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
