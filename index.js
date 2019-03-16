b561691aca2d27c821d182b61da8e1912b12a701
mariam-amer <mariam.ammer98@gmail.com>
2019-03-16 16:48:51 +0200

    added create and read

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//
+const express = require('express')
+
+const admin = require('./routes/api/Admin')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Admin page</h1>
+    <a href="/api/Admin"> View Admin </a>
+    `);
+})
+
+
+
+
+// Direct routes to appropriate files 
+
+app.use('/api/Admin', admin)
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 4000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
--- routes/api/Admin.js
+++ routes/api/Admin.js
@@ -1,1 +1,98 @@
-//
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+router.use(express.json())
+
+const Admins = [
+    {
+    id:'1',
+    name: 'Zefta', 
+    email: 'sama@gmail.com',
+    address: 'tagmo3',
+    username:'sama.zidan',
+     password: 'toty1998'},
+
+    {
+     id: '2',
+    name: 'rawan',
+     email: 'rawan@gmail.com',
+     address: '7elwan',
+      username:'rawraw',
+       password: 'csen604'},
+
+
+    { 
+    id: '3',
+    name: 'hana', 
+    email: 'hana@gmail.com',
+    address: 'madinaty',
+    username:'hanoun', 
+    password: 'csen602'},
+
+
+    {
+     id: '4',
+    name: 'sarah', 
+    email: 'saraseif@gmail.com',
+    address: 'tagmoo3',
+    username:'elcinderalla', 
+    password: 'csen607'}  
+
+
+];
+
+
+
+router.get('/', (req, res) => res.json({ data: Admins }));
+
+
+
+router.post('/', (req, res) => {
+    const name= req.body.name
+    const email= req.body.email
+    const address= req.body.address
+    const username=req.body.username
+    const password=req.body.password
+       
+
+	
+    const schema = {
+		name: Joi.string().alphanum().min(3).max(30).required(),
+        email: Joi.string(),
+        address:Joi.string().required(),
+        username: Joi.string().required(),
+        password: Joi.string().required(),
+
+	}
+
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) return res.status(400).send({ error: result.error.details[0].message });
+
+	
+    
+    const Admin = {
+        
+        name:name,
+        email:email,
+        address:address,
+        username:username,
+        password:password,
+        id: Admins.length + 1
+        
+
+    }
+
+    Admins.push(Admin)
+	return res.json({ data: Admins });
+});
+
+// router.get('/:id', (req, res) => {
+//     const AdminId = req.params.id
+//     const Admin = Admin.find(Admin => Admin.id === AdminId)
+//     return res.json({ data: Admin });
+   
+// })
+
+module.exports = router;
