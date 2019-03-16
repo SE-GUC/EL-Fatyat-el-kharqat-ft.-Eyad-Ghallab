c90d7e555198572edf3d83950a26c496d8f8bbb8
hanahicham <hanahicham25@gmail.com>
2019-03-16 16:51:48 +0200

    I added the update and delete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-//
+const express = require('express')
+
+const Form = require('./routes/api/spc')
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome</h1>
+    <a href="/api/spc">SPC</a>
+    `);
+})
+
+// Direct routes to appropriate files 
+app.use('/api/spc', Form)
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 4000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))//
