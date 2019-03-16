b9615039e428169d016cfc356823b8a898a55ad2
MennaHossam98 <menna.atya@student.guc.edu.eg>
2019-03-16 16:40:18 +0200

    Create&Read Reviewer

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 //
+
+const express = require('express')
+
+const Reviewer = require('./routes/api/reviewer')
+//const admin = require('./routes/api/admin')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Reviewer page</h1>
+    <a href="/api/reviewer"> View Reviewer </a>
+    `);
+})
+
+
+
+
+// Direct routes to appropriate files 
+
+app.use('/api/reviewer', Reviewer)
+
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 4000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
--- routes/api/Reviewer.js
+++ routes/api/Reviewer.js
@@ -1,1 +1,87 @@
 //
+const express = require('express')
+
+//const app = express()
+const router = express.Router()
+router.use(express.json())
+//const Joi=require('joi')
+
+// temporary data created as if it was pulled out of the database ...
+const reviewers = [
+   
+   {Name: "Menna",
+    id:"1",
+    Email:"Menna1@gamil.com",
+    Username:"Menna1",
+    Password:"abc12345",
+    YearsOfExperience: "10",
+    Gender:"Female"},
+
+    {
+        Name: "Ahmed",
+    id:"2",
+    Email:"Ahmed2@gmail.com",
+    Username:"Ahmed2",
+    Password:"defg6789",
+    YearsOfExperience: "11",
+    Gender:"Male"},
+
+    {
+        Name: "Khaled",
+    id:"3",
+    Email:"Khaled3@gmail.com",
+    Username:"Khaled3",
+    Password:"hijklm12",
+    YearsOfExperience: "5",
+    Gender:"Male"}
+    
+    
+    
+
+
+];
+
+router.get('/', (req, res) => res.json({ data: reviewers }));
+router.post('/', (req, res) => {
+	const Name = req.body.Name;
+    const id = req.body.id;
+    const Email = req.body.Email;
+    const Username=req.body.Username;
+    const Password=req.body.Password;
+    const YearsOfExperience = req.body.YearsOfExperience;
+    const Gender = req.body.Gender;
+    
+
+	if (!Name) return res.status(400).send({ err: 'Name field is required' });
+	if (typeof Name !== 'string') return res.status(400).send({ err: 'Invalid value for name' });
+	//if (!ID) return res.status(400).send({ err: 'id field is required' });
+    //if (typeof ID !== 'number') return res.status(400).send({ err: 'Invalid value for id' });
+    if (!Email) return res.status(400).send({ err: 'email field is required' });
+    if (typeof Email !== 'string') return res.status(400).send({ err: 'Invalid value for email' });
+    if (!Username) return res.status(400).send({ err: 'Username field is required' });
+    if (typeof Username !== 'string') return res.status(400).send({ err: 'Invalid value for Username' });
+    if (!Password) return res.status(400).send({ err: 'Password field is required' });
+    if (typeof Password !== 'string') return res.status(400).send({ err: 'Invalid value for Password' });
+    if (!YearsOfExperience) return res.status(400).send({ err: 'yearsofexp field is required' });
+    if (typeof YearsOfExperience !== 'number') return res.status(400).send({ err: 'Invalid value for yearsofexp' });
+    if (!Gender) return res.status(400).send({ err: 'gender field is required' });
+	if (typeof Gender !== 'string') return res.status(400).send({ err: 'Invalid value for gender' });
+
+	const newReviewer = {
+        Name,
+        id:reviewers.length+1,
+        Email,
+        Username,
+        Password,
+        YearsOfExperience,
+        Gender,
+        
+    }
+    
+    reviewers.push(newReviewer)
+    res.send(reviewers) 
+
+});
+
+
+module.exports = router;
