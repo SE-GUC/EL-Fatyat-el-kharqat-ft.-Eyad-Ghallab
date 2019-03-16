6528a0799de61dd8a5e8f35e5fdf8c01fd4687e5
FarahHossam <farah.rashed@student.guc.edu.eg>
2019-03-16 17:36:30 +0200

    Added Create and Read

--- routes/api/investor.js
+++ routes/api/investor.js
@@ -69,6 +69,108 @@ const Investors = [
 
 
 ];
+
+router.get('/', (req, res) => res.json({ data: investors }));
+
+// Create a new user
+// router.post('/', (req, res) => {
+// 	const name = req.body.name;
+// 	const age = req.body.age;
+
+// 	if (!name) return res.status(400).send({ err: 'Name field is required' });
+// 	if (typeof name !== 'string') return res.status(400).send({ err: 'Invalid value for name' });
+// 	if (!age) return res.status(400).send({ err: 'Age field is required' });
+// 	if (typeof age !== 'number') return res.status(400).send({ err: 'Invalid value for age' });
+
+// 	const newUser = {
+// 		name,
+// 		age,
+// 		id: uuid.v4(),
+// 	};
+// 	return res.json({ data: newUser });
+// });
+
+router.post('/joi', (req, res) => {
+	// const name = req.body.name
+    // const age = req.body.age
+    
+    const name= req.body.name
+    const email= req.body.email
+    const nationality=req.body.nationality
+    const gender= req.body.gender
+    const birthdate=req.body.birthdate
+    const address= req.body.address
+    const city= req.body.city
+    const country=req.body.country
+    const jobtitle= req.body.jobtitle
+    const mobilenumber= req.body.mobilenumber
+    const username=req.body.username
+    const password=req.body.password
+       
+
+	// const schema = {
+	// 	name: Joi.string().min(3).required(),
+	// 	age: Joi.number().required(),
+    // }
+    
+    const schema = {
+		name: Joi.string().alphanum().min(3).max(30).required(),
+        email: Joi.string().email().regex(/example.com/),
+        nationality: Joi.string().required(),
+        gender:Joi.string().allow('').optional(),
+        birthdate:Joi.string().required(),
+        address:Joi.string().required(),
+        city: Joi.string().required(),
+        country: Joi.string().required(),
+        jobtitle:Joi.string().allow('').optional(),
+        mobilenumber: Joi.string().trim().regex(/^[0-9]{11,11}$/).required(),
+        username: Joi.string().required(),
+        password: Joi.string().required(),
+
+	}
+
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) return res.status(400).send({ error: result.error.details[0].message });
+
+	// const newUser = {
+	// 	name,
+	// 	age,
+	// 	id: uuid.v4(),
+    // };
+    
+    const investor = {
+       
+        
+        name:name,
+        email:email,
+        nationality:nationality,
+        gender:gender,
+        birthdate:birthdate,
+        address:address,
+        city:city,
+        country:country,
+        jobtitle:jobtitle,
+        mobilenumber:mobilenumber,
+        username:username,
+        password:password,
+        id: investors.length + 1
+
+    }
+
+    investors.push(investor)
+    //res.send(investors)
+	return res.json({ data: investor });
+});
+
+router.get('/:id', (req, res) => {
+    const investorId = req.params.id
+    const investor = investors.find(investor => investor.id === investorId)
+    return res.json({ data: investor });
+    //res.send(investor)
+})
+
 router.put('/:id', (req, res) => {
     const investorId = req.params.id 
     const investor = Investors.find(investor => investor.id === investorId)
