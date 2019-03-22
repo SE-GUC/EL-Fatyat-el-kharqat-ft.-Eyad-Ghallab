34381d9b21eb53d08c0521f82ed8ef037c6c7fbf
FarahHossam <farah.rashed@student.guc.edu.eg>
2019-03-22 21:18:51 +0200

    added create and read

--- Sprint2/Config/keys_dev.js
+++ Sprint2/Config/keys_dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://sama123:sama.1998@se-q3mun.mongodb.net/test?retryWrites=true',
+    //mongoURI: 'mongodb+srv://sama123:sama.1998@se-q3mun.mongodb.net/test?retryWrites=true',
+    mongoURI: 'mongodb+srv://Farah_223:Ff12345678.@cluster0-6nr3w.mongodb.net/test?retryWrites=true', 
 }
--- Sprint2/validations/investorvalid.js
+++ Sprint2/validations/investorvalid.js
@@ -8,6 +8,16 @@ module.exports = {
 
             username: Joi.string().required(),
             password: Joi.string().min(3).max(100).required(),
+ 
+        nationality: Joi.string().required(),
+        gender:Joi.string().allow('').optional(),
+        birthdate:Joi.string().required(),
+        address:Joi.string().required(),
+        city: Joi.string().required(),
+        country: Joi.string().required(),
+        jobtitle:Joi.string().allow('').optional(),
+        mobilenumber: Joi.string().trim().regex(/^[0-9]{11,11}$/).required(),
+       
         }
 
         return Joi.validate(request, createSchema)
@@ -20,6 +30,15 @@ module.exports = {
   
             username: Joi.string(),
             password: Joi.string().min(3).max(100),
+
+        nationality: Joi.string().required(),
+        gender:Joi.string().allow('').optional(),
+        birthdate:Joi.string().required(),
+        address:Joi.string().required(),
+        city: Joi.string().required(),
+        country: Joi.string().required(),
+        jobtitle:Joi.string().allow('').optional(),
+        mobilenumber: Joi.string().trim().regex(/^[0-9]{11,11}$/).required(),
 
         }
 
