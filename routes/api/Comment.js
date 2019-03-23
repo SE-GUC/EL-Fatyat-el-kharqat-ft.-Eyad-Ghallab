8b5ea59f11dfc61fcbad4918b69817768d6e3996
samazidan <sama.1998.zidan@gmail.com>
2019-03-23 22:26:24 +0200

    changed comment

--- routes/api/Comment.js
+++ routes/api/Comment.js
@@ -2,52 +2,62 @@ const express = require('express');
 const router = express.Router();
 const validator = require('../../validations/Commentvalid')
 //  Comment
-const Comment = require('../../models/Comment');
+const Comment1 = require('../../models/Comment');
 
 // @route   GET api/Comment
 // @desc    Get All Comment
 // @access  Public
 
 router.get('/', async (req,res) => {
-  const Comment = await Comment.find()
+  const Comment = await Comment1.find()
   res.json({data : Comment})
 })
 router.get('/:id', async (req,res) => {
-  const Comment = await Comment.findById(req.params.id)
+  const Comment = await Comment1.findById(req.params.id)
  res.json({data : Comment})
 })
 // @route   POST api/Comment
 // @desc    Create An Comment
 // @access  Public
-router.post('/', (req, res) => {
-  const newComment = new Comment({
-    id:req.body.id,
-    name: req.body.name,
-    comment: req.body.comment
-  });
-
-  newComment.save().then(Comment => res.json(Comment));
-});
+router.post('/', async (req,res) => {
+    try {
+     const isValidated = validator.createValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const Comment = await Comment1.create(req.body)
+     res.json({msg:'Comment was created successfully', data: Comment})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+ })
 
 // @route   DELETE api/Comment/:id
 // @desc    Delete A Comment
 // @access  Public
-router.delete('/:id', (req, res) => {
-    Comment.findById(req.params.id)
-    .then(Comment => Comment.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
-});
-
-// Update a comment
-router.put('/:id',async (req,res) => {
+router.delete('/:id', async (req,res) => {
     try {
      const id = req.params.id
-     const forma = await Comment.findOne({id})
-     if(!Comment) return res.status(404).send({error: 'comment does not exist'})
-     const isValidated = validator.updateValidation(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const updatedBook = await Comment.updateOne(req.body)
-     res.json({msg: 'comment updated successfully'})
+     const deletedComment = await Comment1.findByIdAndRemove(id)
+     res.json({msg:'Comment was deleted successfully', data: deletedComment})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+ })
+
+// Update a comment
+
+router.put('/:id', async (req,res) => {
+    try {
+     const id = req.params.id
+    //  const investor = await inv.findOne({id})
+    //  if(!investor) return res.status(404).send({error: 'investor does not exist'})
+    //  const isValidated = validator.updateValidation(req.body)
+    //  if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const updatedcom = await Comment1.updateOne(req.body)
+     res.json({msg: 'Comment updated successfully', data: updatedcom})
     }
     catch(error) {
         // We will be handling the error later
--- validations/Commentvalid.js
+++ validations/Commentvalid.js
@@ -1,11 +1,32 @@
 const Joi = require('joi')
 module.exports = {
+
+
+
+
+    createValidation: request => {
+        const createSchema = {
+            name: Joi.string().min(3).max(500).required(),
+            
+
+            comment: Joi.string().required(),
+    
+            date:Joi.date().required()
+            
+
+        }
+
+        return Joi.validate(request, createSchema)
+    },
 updateValidation: request => {
         const updateSchema = {
             name: Joi.string().min(3).max(500),
             comment: Joi.string(),
-            id: Joi.string()
+            date:Joi.date().required()
+            // id: Joi.string()
         }
+
+
 
         return Joi.validate(request, updateSchema)
     }, 
