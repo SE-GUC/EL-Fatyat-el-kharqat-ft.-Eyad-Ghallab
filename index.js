39a5f6358fbf542691388d2ce182524f10673319
samazidan <sama.1998.zidan@gmail.com>
2019-03-23 18:01:27 +0200

    changed from investors to investor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 
-const investor = require('./routes/api/investors')
+const investor = require('./routes/api/investor')
 
 
 const app = express()
