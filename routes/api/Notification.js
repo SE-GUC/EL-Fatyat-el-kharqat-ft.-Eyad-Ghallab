84651c49b32834ed241bc17958e8e3e7dabf6df9
SarahSeif <sarah388320@gmail.com>
2019-03-23 18:02:21 +0200

    I added notification and create and read

--- routes/api/Notification.js
+++ routes/api/Notification.js
@@ -3,13 +3,37 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 const notification = require('../../models/Notification')
-const validator = require('../../validations/notificationvalidation')
+const validator = require('../../validations/Notificationvalid')
+
+router.get('/', async (req,res) => {
+    const Notifications = await notification.find()
+    res.json({data: Notifications})
+})
+
+router.get('/:User_id', async (req,res) => {
+    const User_id = req.params.User_id
+    const Notifications = await notification.find({User_id})
+    res.json({data: Notifications})
+})
+
+
+router.post('/', async (req,res) => {
+    try {
+     const isValidated = validator.createValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const newnotification = await notification.create(req.body)
+     res.json({msg:'notification was created successfully', data: newnotification})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+ })
 
 
 
 
-
-// Update a book
+// Update a notification
 router.put('/:id', async (req,res) => {
     try {
      //const id = req.params.id
@@ -30,7 +54,7 @@ router.put('/:id', async (req,res) => {
     try {
      const id = req.params.id
      const deletednotification = await notification.findByIdAndRemove(id)
-     res.json({msg:'Book was deleted successfully', data: deletednotification})
+     res.json({msg:'notification was deleted successfully', data: deletednotification})
     }
     catch(error) {
         // We will be handling the error later
