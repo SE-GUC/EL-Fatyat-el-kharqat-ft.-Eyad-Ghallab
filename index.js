aea1d08f73190c609cb2bac0044a0831db18004c
Aliaaabouelhag <aliaaabouelhag@gmail.com>
2019-03-23 17:53:01 +0200

    added crud

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-//const express = require('express')
-
-
-const SSC = require('./routes/api/SSC')
+const express = require('express')
+const mongoose = require('mongoose')
+const updateSSC = require('./routes/api/SSC')
 
 const app = express()
+
+const db = require('./config/keys').mongoURI
+
+
+mongoose
+    .connect(db)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
+
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome</h1>
-    <a href="/api/SSC">SSC FORM</a>
-    `);
-})
 
-// Direct routes to appropriate files 
+app.get('/', (req,res) => res.send(`<h1>Sumerge forms</h1>`))
+app.get('/test', (req,res) => res.send(`<h1>Deployed on Heroku</h1>`))
 
-app.use('/api/SSC', SSC)
+app.use('/api/SSC', updateSSC)
 
-// Handling 404
-app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
- })
 
-const port = 4000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server on ${port}`))
---
+++ models/SSC.js
@@ -0,0 +1,129 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const SSCSchema = new Schema({
+   
+    Company_name: {
+        type: String,
+        required: true
+    },
+    Governorate: {
+        type: String,
+        required: true
+    },
+    City: {
+        type: String, 
+        required: true
+    },
+    reviews: {
+        type: [String]
+    },
+    Company_Address: {
+        type: String, 
+        required: true
+    },
+    Company_Phone_Number: {
+        type: String, 
+        required: true
+    },
+    Fax: {
+        type: String, 
+        required: true
+    },
+    Capital_Currency: {
+        type: String, 
+        required: true
+    },
+    capital: {
+        type: Number, 
+        required: true
+    },
+    investorname: {
+        type: String, 
+        required: true
+    },
+    Investor_type: {
+        type: String, 
+        required: true
+    },
+    Gender: {
+        type: String, 
+        required: true
+    },
+    Nationality: {
+        type: String, 
+        required: true
+    },
+    TypeOf_IdentityProof: {
+        type: String, 
+        required: true
+    },
+    investor_nationalid: {
+        type: String, 
+        required: true
+    },
+    BirthDate: {
+        type: String, 
+        required: true
+    },
+    Address: {
+        type: String, 
+        required: true
+    },
+    Phone_Number: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    BOD_Name: {
+        type: String, 
+        required: true
+    },
+    BOD_Investor_Type: {
+        type: String, 
+        required: true
+    },
+    BOD_Gender: {
+        type: String, 
+        required: true
+    },
+    BOD_Nationality: {
+        type: String, 
+        required: true
+    },
+    BOD_TypeOfIdentityProof: {
+        type: String, 
+        required: true
+    },
+    BOD_NationalID: {
+        type: String, 
+        required: true
+    },
+    BOD_BirthDate: {
+        type: String, 
+        required: true
+    },
+    BOD_Address: {
+        type: String, 
+        required: true
+    },
+    PositionInBOD: {
+        type: String, 
+        required: true
+    },
+   Formdate:{
+       type:String,
+       required: true
+   },
+   Lock:{
+       type:Boolean,
+       required:true
+   }
+
+
+})
+
+module.exports = SSC = mongoose.model('SSC', SSCSchema)
