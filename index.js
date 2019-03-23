b8638f572070c8348440fdd5b56e6c32f15d7b71
ghallab97 <mohamed.ghalab@student.guc.edu.eg>
2019-03-23 18:09:18 +0200

    crud of comments

--- index.js
+++ index.js
@@ -1,78 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const app = express();
 
+const Comment = require("./routes/api/Comment");
 
-const express = require('express')
-const app = express()
-app.use(express.json())
+// Bodyparser Middleware
+app.use(bodyParser.json());
 
+// DB Config
+const db = require("./config/keys").mongoURI;
 
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true }) // Adding new mongo url parser
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+mongoose.set("useCreateIndex", true);
 
+// Use routes
+app.use("/api/Comment", Comment);
 
+const port = process.env.PORT || 4000;
 
-
-
-
-
-const Investors = require('./routes/api/investor')
-
-
-
-const Reviewer = require('./routes/api/reviewer')
-
-const SSC = require('./routes/api/SSC')
-
-
-
-const admin = require('./routes/api/Admin')
-
-
-
-const Form = require('./routes/api/SPC')
-
-
-const Lawyer= require('./routes/api/Lawyer')
-
-app.get('/', (req, res) => {
-
-    res.send(`<h1>Welcome to Sumerge page</h1>
-    <a href="/api/reviewer"> View Reviewer </a>
-
-    `);
-})
-
-
-
-
-// Direct routes to appropriate files 
-
-app.use('/api/reviewer', Reviewer)
-
-
-
-
-
-// Direct routes to appropriate files 
-app.use('/api/Lawyer', Lawyer)
-
-//app.use('/api/books', books)
-
-app.use('/api/SSC', SSC)
-app.use('/api/Admin', admin)
-
-
-// Direct routes to appropriate files 
-app.use('/api/SPC', Form)
-
-
-// Direct routes to appropriate files 
-
-app.use('/api/investor', Investors)
-
-// Handling 404
-app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
- })
-
-const port = 4000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
-
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const commentSchema = new Schema({
+  id: {
+    type: String,
+    required:true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  comment: {
+    type: String,
+    required:true
+  }
+ 
+});
+
+module.exports = form = mongoose.model('form', commentSchema);
