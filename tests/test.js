bec8f9cafae3429917c13d0ef3b3725b5f760732
mariam-amer <mariam.ammer98@gmail.com>
2019-03-31 19:37:55 +0200

    test updated

--- tests/test.js
+++ tests/test.js
@@ -2,10 +2,20 @@ const funcs = require('./functions.js');
 
 test('get External Entity', async () => {
   const Entity =  await funcs.GetEntity();
+  expect(Entity.data.data.length).toBeGreaterThan(0);
 });
+
 test('post  External Entity', async () => {
-  const Entity =  await funcs.PostEntity();
-expect(Entity.data.msg).toEqual("Entity was created successfully");
+  const Entity =  await funcs.GetEntity();
+  const oldlength = Entity.data.data.length;
+  const response1 = await funcs.PostEntity({
+    "name":"Mary",
+    "username":"ammar_ah",
+    "password":"ahumkcnsjkabjhbssnaknsdk",
+  })
+expect(response1.data.msg).toEqual("Entity was created successfully");
+const response2 = await funcs.GetEntity();
+expect(response2.data.data.length).toEqual(oldlength +1);
 });
 
 test(' update External Entity', async () => {
