ed6d6d542b51158e09a5ce446299616b555acbe6
unknown <menna.atya@student.guc.edu.eg>
2019-03-31 22:25:10 +0200

    tests c&R

--- fn.js
+++ fn.js
@@ -1,6 +1,15 @@
 const axios = require("axios");
 axios.defaults.adapter = require("axios/lib/adapters/http");
 const functions= {
+  getReviewer: async () => {
+        const Reviewer = await axios.get('http://localhost:3000/api/Reviewer/');
+        return Reviewer;
+        },
+
+    createReviewer: async requestBody => {
+        const Reviewer = await axios.post('http://localhost:3000/api/Reviewer/',requestBody);
+            return Reviewer;
+            },
 
      
     UpdateReviewer: async ()=>{
--- test.js
+++ test.js
@@ -1,4 +1,37 @@
-const functions = require("../functions/reviewer.functions");
+const functions = require('./fn');
+
+test("Getting all reviewers", async () => {
+    expect.assertions(1);
+    const allReviewers = await functions.getReviewer();
+    expect(allReviewers.data.msg).toEqual("these are the reviewers");
+    
+});
+
+
+
+
+
+
+  test ("creating Reviewers",async () => {
+    const response= await functions.getReviewer();
+    const oldLength = response.data.data.length;  
+    const response1 = await functions.createReviewer({
+        "Name":"MennaHossam",
+        "Email":"MennaHossam127@gmail.com",
+        "Username":"Menna",
+       " Password": "12345",
+        "YearsOfExperience": "412",
+        "Gender": "Female"
+
+      })
+      expect(response1.data.msg).toEqual('Reviewer was created successfully')
+      const response2 = await functions.getReviewer();
+    
+      expect(response2.data.data.length).toEqual(oldLength + 1);
+      
+      },100000);
+
+
 test("update Reviewer", async()=>{
     expect.assertions(1);
     const response = await functions.UpdateReviewer();
