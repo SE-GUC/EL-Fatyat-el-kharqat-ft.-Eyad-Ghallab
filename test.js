9d3633a4d2bec776a89c2693738fc93bc6bac80c
Eyadsaeda <eyad.seada@student.guc.edu.eg>
2019-03-31 21:47:25 +0200

    yarab

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-const functions = require("../functions/payment.functions");
+const functions = require("./fn");
 test("update payment", async()=>{
     expect.assertions(1);
     const response = await functions.UpdatePayment();
-    expect(response.data.msg).toEqual("Payment is updated successfully");
+    expect(response.data.msg).toEqual("payment updated successfully");
 });
-
-//5c9fa97cf853175cc446dd29
-test("Delete Payment",async()=>{
+test("Delete payment",async()=>{
     expect.assertions(1);
     const response = await functions.Deletepayment();
     expect(response.data.msg).toEqual("payment was deleted successfully");
 });
-    
