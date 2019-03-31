1bf627a3d648885cdd033d42f3a14b946d59a861
NorhanTarek98 <norhan.elnaghy@student.guc.edu.eg>
2019-03-31 22:35:18 +0200

    testing

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 const lawyer_funcs = require ('./fn');
 
+
 test("Creating a lawyer", async()=>{
     expect.assertions(2);
     const response= await lawyer_funcs.getLawyers();
@@ -22,7 +23,7 @@ test("Getting all lawyers", async () => {
 })
 test("Getting unloced ssc", async () => {
     expect.assertions(1);
-    const allssc = await funcs.getSSC()
+    const allssc = await lawyer_funcs.getSSC()
     expect(allssc.data.data.length).toBeGreaterThan(0)
 
     //expect(allssc.data.data.Loceked).toEqual(false)
@@ -30,44 +31,19 @@ test("Getting unloced ssc", async () => {
 })
 test("Getting unloced spc", async () => {
     expect.assertions(1);
-    const allspc = await funcs.getSPC()
+    const allspc = await lawyer_funcs.getSPC()
     //expect(allspc.data.data.Loceked).toEqual(false)
     expect(allspc.data.data.length).toBeGreaterThan(0)
 
     
 })
-test("Creating a contract then updating it", async ()=>{
-    //   expect.assertions(2);
-       //const response= await Inv_funcs.getInvestors();
-      // const oldLength = response.data.data.length;  
-       const created = await funcs.createLawyer({
-        "username": "rawan220698",
-        "password": "rawan222222",
-        "fullname": "rawan essily",
-        "email": "rawan@gmail.com",
-        "phonenumber": "01122251595",
-        "legaltype": "business",
-        "birthdate": "6/22/1960",
-        "noOfPreviousCases": 57,
-        "gender": "female",
-    });
-        const updated =  {username: "rawanHossam"}
-        const response1= await funcs.updateLawyer(created.data.data._id,updated)
 
-             const allLawyer = await funcs.getlawyerbyid(created.data.data._id)
-            //const response3= await Inv_funcs.getInvestors();
-           // response3= await Inv_funcs.getInvestors();
-          expect(allLawyer.data.data.username).toEqual("rawanHossam");
-           expect(allLawyer.data.data.phonenumber).toEqual(created.data.data.phonenumber);
-
-        // });
-        
-        },100000);
+   
 test("Creating an lawyer then deleting it", async ()=>{
     expect.assertions(1);
-    const response= await funcs.getLawyer();
+    const response= await lawyer_funcs.getLawyer();
     const oldLength = response.data.data.length;  
-    const response1 = await funcs.createLawyer({
+    const response1 = await lawyer_funcs.createLawyer({
         "username": "Hana",
         "password": "rawan222222",
         "fullname": "rawan essily",
@@ -83,9 +59,9 @@ test("Creating an lawyer then deleting it", async ()=>{
     //expect(response2.data.data.length).toEqual(oldLength + 1);
     
 
-    const response2= await funcs.deleteLawyer(
+    const response2= await lawyer_funcs.deleteLawyer(
         response1.data.data._id);
-        response3= await funcs.getLawyer();
+        response3= await lawyer_funcs.getLawyer();
         expect(response3.data.data.length).toEqual(oldLength);
 
     // });
