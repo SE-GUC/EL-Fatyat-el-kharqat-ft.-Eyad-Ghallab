666fd3025a466c60bbe52f5f4249a53067ec26a5
mariam-amer <mariam.ammer98@gmail.com>
2019-04-06 17:48:58 +0200

    get and create

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import SPC from './components/SPC/SPC';
-import Investor from './components/Investor/Investor';
+import ExternalEntities from './components/ExternalEntities/ExternalEntities';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-        <SPC/>
-     <Investor/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <ExternalEntities/>
       </div>
     );
   }
 }
 
 export default App;
+
+
