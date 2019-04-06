b0c77e66c3514e038bf6ff56fa7d328698bb2edc
hanahicham <hanahicham25@gmail.com>
2019-04-06 11:29:20 +0200

    SPC Front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SPC from './components/SPC/SPC';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <SPC/>
       </div>
     );
   }
