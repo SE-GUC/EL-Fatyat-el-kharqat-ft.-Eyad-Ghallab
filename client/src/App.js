da18e5b80c50bd290fae5788ef5a470f185342e4
Eyadsaeda <eyad.seada@student.guc.edu.eg>
2019-04-12 16:41:33 +0200

    ha

--- client/src/App.js
+++ client/src/App.js
@@ -63,6 +63,39 @@ class App extends Component {
             </div>
           </nav> <br/>
           <h2>Welcome to GAFI</h2> <br/>
+          <h3>About Us</h3> <br/>
+          <label>
+          GAFI is an affiliate of the Ministry of Investment (MOI) and the principal government body regulating and facilitating investment in Egypt.<br/>
+Although the authority retains its traditional regulatory powers, GAFI today is an effective, proactive investment promotion agency with promotion, facilitation, business matchmaking, Egyptian expatriates, events, investor aftercare, and research and market intelligence functions.<br/>
+As Egypt's one-stop shop for investment, GAFI eases the way for global investors looking to harness opportunities presented by Egypt's fast-growing domestic economy and the nation's robust competitive advantages as an export hub for Europe, the Arab world and Africa.
+<br/>
+GAFI's investor care officers also ensure that client feedback is channeled into government agencies with a view towards continuously enhancing and reforming Egypt's investment climate.<br/> In addition to promoting Egypt's investment opportunities in various sectors, <br/>GAFI is launching new initiatives aimed at promoting the investment climate in Egypt including the adoption of new investment regimes (investment zones and special economic zones) and the establishment of the SME Entrepreneurial Center and Fund (Bedaya).
+</label>
+
+
+<h4>OUR MISSION</h4>
+<label>
+"To enable and sustain Egypt's economic growth through investment promotion, facilitation, efficient business services and advocacy of investor friendly policies "
+</label>
+
+<h4>Contact us On:</h4>
+<label>Adress:
+  No. 3, Salah Salem st., Nasr City, Cairo, 11562, Egypt.
+</label><br/>
+<label>telephone number:
++202 240 55 452
+</label><br/>
+<label>FAX:
+ 16035</label>
+ <br/>
+
+ 
+ 
+ <form action="https://www.Facebook.com/">
+         <button type="submit">Facebook<img src="" /></button>
+      </form>
+
+
           <Switch>
               <Route exact path='/SPCHomePage' component={ SPCHomePage } />
               <Route exact path='/Admin' component={ Admin} />
