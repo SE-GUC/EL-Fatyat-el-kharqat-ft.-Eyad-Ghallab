fd440259e1b383d9f52adfbe8161e2b63d929375
SarahSeif <sarah388320@gmail.com>
2019-04-20 21:36:15 +0200

    added

--- client/src/components/Lawyer/LawyerHomePage.js
+++ client/src/components/Lawyer/LawyerHomePage.js
@@ -6,45 +6,33 @@ import EditLawyer from './EditLawyer';
 import DeleteLawyer from './DeleteLawyer';
 import CreatingLawyer from './CreatingLawyer';
 
-
+import './Lawyer.css';
 
 
 export default class LawyerHomePage extends Component {
     render() {
       return (
-        <Router>
-          <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <Link to={'/'} className="navbar-brand">Sumerge</Link>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <Link to={'/'} className="nav-link">Home</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to={'/create'} className="nav-link">Create</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to={'/edit'} className="nav-link">Edit</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to={'/delete'} className="nav-link">Delete </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to={'/read'} className="nav-link">Lawyer </Link>
-                  </li>
-                </ul>
-              </div>
-            </nav> <br/>
-            <h2>Welcome to GAFI</h2> <br/>
+        <Router>      
+<div className="navbar navbar-expand-lg navbar-light bg-light" id="cssmenu"><div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>
+<ul>
+   <li class="active"><span><Link to={'/'}>GAFI</Link></span></li>
+   <li><span> <Link to={'/'}>Home</Link></span></li>
+   <li><span> <Link to={'/create'} className="nav-link">Create</Link></span></li>
+   <li><span><Link to={'/edit'} className="nav-link">Edit</Link></span></li>
+   <li><span> <Link to={'/delete'} className="nav-link">Delete </Link></span></li>
+   <li class="last"><span> <Link to={'/read'} className="nav-link">Lawyer </Link></span></li>
+</ul>
+<h2>Welcome to GAFI</h2> <br/>
             <Switch>
                 <Route exact path='/create' component={ CreatingLawyer } />
                 <Route exact path='/edit' component={ EditLawyer } />
                 <Route exact path='/delete' component={ DeleteLawyer} /> 
                 <Route exact path='/read' component={ Lawyer } /> 
             </Switch>
-          </div>
+</div>
+          
         </Router>
+        
       );
     }
   }
