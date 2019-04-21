1641a81a4a79e78250a03215112e2c59c3c3f828
samazidan <sama.1998.zidan@gmail.com>
2019-04-21 15:24:59 +0200

    spc and ssc

--- client/src/components/Investor/mycompSPC.js
+++ client/src/components/Investor/mycompSPC.js
@@ -10,8 +10,6 @@ class mycompSPC extends Component {
   }
 
   componentDidMount() {
-    console.log("hiiii");
-
     fetch("/api/SPC/" + localStorage.getItem("nationalid") + "/SPC")
       .then(res => res.json())
       .then(spc =>
@@ -19,9 +17,11 @@ class mycompSPC extends Component {
           console.log("My SPC Comapnies", this.state.SPC)
         )
       );
-    if (this.state.SPC != undefined) {
-      this.setState.isSPC = true;
+    if (this.state.SPC != []) {
+      this.setState({ isSPC: true });
     }
+
+    console.log(this.state.isSPC);
   }
   render() {
     // const pro = this.getpro();
@@ -31,62 +31,54 @@ class mycompSPC extends Component {
       <div>
         {this.state.isSPC ? (
           <form>
-            <h2>My Companies</h2>
-            <h1> Facility name: {this.state.SPC.Facility_name} </h1>
-            <br />
-            <h1>
-              {" "}
-              Facility name in English: {this.state.SPC.Facility_nameinenglish}
-            </h1>
-            <br />
-            <h1> Governorate: {this.state.SPC.Governorate}</h1>
-            <br />
-            <h1>City: {this.state.SPC.City}</h1>
-            <br />
-            <h1>Facility Address: {this.state.SPC.Facility_Address}</h1>
-            <br />
-            <h1>
-              {" "}
-              Facility Phone Number:: {this.state.SPC.Facility_Phone_Number}
-            </h1>
-            <br />
-            <h1> Fax: {this.state.SPC.Fax}</h1>
-            <br />
-            <h1> Capital Currency: {this.state.SPC.Capital_Currency}</h1>
-            <br />
-            <h1>capital: {this.state.SPC.capital}</h1>
-            <br />
-            <h1>investorname: {this.state.SPC.investorname}</h1>
-            <br />
-            <h1> Gender: {this.state.SPC.Gender}</h1>
-            <br />
-            <h1> Nationality: {this.state.SPC.Nationality}</h1>
-            <br />
-            <h1> ypeOf IdentityProof: {this.state.SPC.ypeOf_IdentityProof}</h1>
-            <br />
-            <h1> investor nationalid: {this.state.SPC.investor_nationalid}</h1>
-            <br />
-            <h1> Birth Date: {this.state.SPC.BirthDate}</h1>
-            <br />
-            <h1> Phone Number: {this.state.SPC.Phone_Number}</h1>
-            <br />
-            <h1> Investor Fax: {this.state.SPC.Investor_Fax}</h1>
-            <br />
-            <h1> email: {this.state.SPC.email}</h1>
-            <br />
-            <h1> Investor Address: {this.state.SPC.Investor_Address}</h1>
-            <br />
-            <h1> Form Date: {this.state.SPC.Form_Date}</h1>
-            <br />
-            <h1> Locked: {this.state.SPC.Locked}</h1>
-            <br />
-            <h1> Status : {this.state.SPC.Status}</h1>
-            <br />
-            <h1>
-              {" "}
-              is the external entities notified:{" "}
-              {this.state.SPC.is_the_external_entities_notified}
-            </h1>
+            <ul>
+              {this.state.SPC.map(SPC => (
+                <li key={SPC._id}>
+                  {" "}
+                  Facility name: {SPC.Facility_name}
+                  <br />
+                  Facility name in English: {SPC.Facility_nameinenglish}
+                  <br />
+                  Governorate: {SPC.Governorate}
+                  <br />
+                  City: {SPC.City}
+                  <br />
+                  Facility Address: {SPC.Facility_Address}
+                  <br />
+                  Facility Phone Number:: {SPC.Facility_Phone_Number}
+                  <br />
+                  Fax: {SPC.Fax}
+                  <br />
+                  Capital Currency: {SPC.Capital_Currency}
+                  <br />
+                  capital: {SPC.capital}
+                  <br />
+                  investorname: {SPC.investorname}
+                  <br />
+                  Gender: {SPC.Gender}
+                  <br />
+                  Nationality: {SPC.Nationality}
+                  <br />
+                  TypeOf IdentityProof: {SPC.TypeOf_IdentityProof}
+                  <br />
+                  investor nationalid: {SPC.investor_nationalid}
+                  <br />
+                  Birth Date: {SPC.BirthDate}
+                  <br />
+                  Phone Number: {SPC.Phone_Number}
+                  <br />
+                  Investor Fax: {SPC.Investor_Fax}
+                  <br />
+                  email: {SPC.email}
+                  <br />
+                  Investor Address: {SPC.Investor_Address}
+                  <br />
+                  Form Date: {SPC.Form_Date}
+                  <br />
+                  Status : {SPC.Status}
+                </li>
+              ))}
+            </ul>
           </form>
         ) : (
           <h1> You dont have any Companies yet</h1>
